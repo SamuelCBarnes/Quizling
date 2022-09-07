5cb2c7c1fdeeea6bb1b9a40b5b068800b8ab0ea7
SamuelCBarnes <samuelcbarnes@gmail.com>
2022-09-07 15:01:23 -0400

    initializing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Data from "./data";
 
-function App() {
+const App = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  const [showData, setShowData] = useState([]);
+
+  const loadData = () => {
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result) {
+          setShowData(result);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div class="main-title">
+        <h5>React Accordian</h5>
+      </div>
+
+      {/* <h1>{JSON.stringify(showData)}</h1> */}
+
+      <div className="row">
+        {showData.map((item, index) => (
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6" key={index}>
+            <Data {...item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
